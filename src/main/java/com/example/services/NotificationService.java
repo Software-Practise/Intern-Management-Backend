1b30f4d18784fdbed00910c09d7c2feb55ee2b80
preshiousshittu <54418932+preshiousshittu@users.noreply.github.com>
2022-03-29 14:38:10 -0500

    finishes Notification method

--- src/main/java/com/example/services/NotificationService.java
+++ src/main/java/com/example/services/NotificationService.java
@@ -3,6 +3,7 @@ import com.example.model.UserModel;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 @Service
@@ -15,8 +16,14 @@ public class NotificationService {
         this.emailSender = emailSender;
 
     }
-    public void sendNotification(UserModel user){
+    public void sendNotification(UserModel user) throws MailException{
         SimpleMailMessage mail = new SimpleMailMessage();
+        mail.setTo(user.getEmail());
+        mail.setFrom("helpmetest2022@outlook.com");
+        mail.setSubject("Your Status has changed");
+        mail.setText("email notification");
+
+        emailSender.send(mail);
     }
 
     
